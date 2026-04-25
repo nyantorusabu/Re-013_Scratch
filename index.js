571f43942fe867e87b6efe10692ba6950abc647c
nyantorusabu <nyantorusabu@outlook.jp>
2026-04-25 17:12:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 (async () => {
 	const SRC = 'https://nyantorusabu.github.io/Re-013_Scratch';
 	async function LoadAssets() {
+		if (!window.NDT) {
+			console.log('🛠️ NDTを読み込み中...');
+			await new Promise((resolve, reject) => {
+				const script = document.createElement('script');
+				script.src = `https://nyantorusabu.github.io/NDT/NekoDevTools.js`;
+				script.onload = resolve;
+				script.onerror = () =>
+					reject(new Error('Loaderの読み込みに失敗しました'));
+				document.body.appendChild(script);
+			});
+		}
 		if (!window.Scratch) {
 			console.log('🛠️ Loaderを読み込み中...');
 			await new Promise((resolve, reject) => {
